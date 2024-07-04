34cf10f6396abd26d437bf10139fe6be5bb0756b
Jin <53550425+jchris27@users.noreply.github.com>
2024-07-04 14:33:18 +0900

    Completed added new users and simple authentication with bcrypt.

--- routes/root.js
+++ routes/root.js
@@ -13,13 +13,4 @@ router.get('^/$|/index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
-router.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'new-page.html'));
-});
-
-// redirect page
-router.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, '/new-page.html'); //302 by default
-});
-
 module.exports = router;
